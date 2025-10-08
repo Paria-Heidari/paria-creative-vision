b8dcdd44447e2fdccbe3da843df2f6c0866a8f96
Paria_ <paria.heidari@knowit.no>
2025-10-08 13:34:11 +0200

    code cleanup & structuring

---
+++ src/components/Body/bodyLayout.tsx
@@ -0,0 +1,3 @@
+export default function BodyLayout(props: { children: React.ReactNode }) {
+  return <>{props.children}</>;
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      Header
+    </header>
+  );
+}
