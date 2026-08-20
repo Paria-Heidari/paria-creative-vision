67af230d529d26aae4a6606ed98d3815d159372e
Paria <35240020+Paria-Heidari@users.noreply.github.com>
2026-08-20 19:06:09 +0200

    build: add vitest and update dev dependencies

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    css: false,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  },
+});
